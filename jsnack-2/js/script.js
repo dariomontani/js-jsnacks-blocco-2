bb87b6d43b52a2b1e48c35de385f27306cf58a74
dariomontani <dariomontani@gmail.com>
2021-11-30 14:29:09 +0100

    jnack 2

--- jsnack-2/js/script.js
+++ jsnack-2/js/script.js
@@ -1,3 +1,28 @@
 // jsnack 2
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
+const numeri = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
+];
+
+let even = 0;
+let odd = 0;
+for (let i = 0; i < numeri.length; i++) {
+
+    if (i % 2 == 0) {
+        even += numeri[i];
+    } else {
+        odd += numeri[i];
+    }
+}
+
+console.log(even);
